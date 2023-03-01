754e7f7bf22d19083f2f78463458b6dde723eb14
Dawid <kreft.kontakt@gmail.com>
2023-03-01 22:12:28 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+const button= document.querySelector('.share-button')
+
+
+
+async function copyText(e) {
+//prevent button going to the site
+    e.preventDefault()
+    const link = this.getAttribute('link')
+    console.log(link)
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Copied the text: " + link)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+shareButtons.forEach(shareButton =>
+    shareButton.addEventListener('click', copyText))
+
+button.addEventListener('click', copyText)
